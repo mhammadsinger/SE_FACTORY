42a2623ac8af43f4f126ad547eb3756726a91ecb
mhammadsinger <117460892+mhammadsinger@users.noreply.github.com>
2023-09-22 08:38:19 +0300

    Delete hook_again directory

--- hook_again/SQL_Commands/V1_create_schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS dw_reporting;
--- hook_again/logging_handler.py
+++
@@ -1,3 +0,0 @@
-def show_error_message(error_string_prefix,error_string_suffix ):
-    error_message = error_string_prefix + "=" + error_string_suffix
-    print(error_message)
